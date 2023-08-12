d92d6cf075931824f9d38894bbd838d53167c7ed
Meruert <meru.forwork@gmail.com>
2023-08-12 22:47:23 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const couple = {};
+
+  bracketsConfig.forEach(config => {
+    couple[config[1]] = config[0];
+  });
+  for (const char of str) {
+    if (char in couple) {
+      if (stack.length === 0 || stack[stack.length - 1] !== couple[char]) {
+        stack.push(char);
+      } else {
+        stack.pop();
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.length === 0;
 }
+
+
+// module.exports = 
